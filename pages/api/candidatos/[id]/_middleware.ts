5f9683b80a64c1b8fb589b39b993ccaa34adf9e3
imSoft <weareimsoft@gmail.com>
2022-09-01 02:11:58 -0500

    Correccion de errores Middleware

--- pages/api/candidatos/[id]/_middleware.ts
+++ pages/api/candidatos/[id]/_middleware.ts
@@ -1,19 +1,23 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextFetchEvent, NextResponse } from 'next/server';
 
-export function middleware ( req: NextRequest, ev: NextFetchEvent ) {
 
-    const id = req.page.params?.id || '';
+export function middleware( req: NextRequest, ev: NextFetchEvent ) {
 
-    const checkMongoIdRegExp = new RegExp('^[0-9a-fA-F]{24}$');
+    // if (req.page.name === '/api/entries') return NextResponse.next();
 
-    if( !checkMongoIdRegExp.test( id ) ) {
-        return new Response( JSON.stringify({ message: 'El Id no es valido ' + id }), {
-            status: 400,
-            headers: {
-                'Content-Type':'application/json'
-            }
-        } );
-    }
+    // const id = req.page.params?.id || '';
+
+    // const checkMongoIDRegExp = new RegExp('^[0-9a-fA-F]{24}$');
+
+    // if ( !checkMongoIDRegExp.test( id ) ) {
+    //     // return res.status(400).json({ message: 'El id no es válido ' + id })
+    //     return new Response( JSON.stringify({ message: 'El id no es válido ' + id }), {
+    //         status: 400,
+    //         headers: {
+    //             'Content-Type':'application/json'
+    //         }
+    //     });
+    // }
 
     return NextResponse.next();
 
