ee9af8f04da3b7e974390f759f455af8e5b841e0
imSoft <weareimsoft@gmail.com>
2023-04-19 00:46:53 -0600

    Se resolvio un bug en la interfaz de reporte de compra

--- interfaces/reporteDeGanancia.ts
+++ interfaces/reporteDeGanancia.ts
@@ -2,7 +2,7 @@ export interface ReporteDeGanancia {
   _id: string;
   mes: string;
   anio: string;
-  sucursal: string;
+  sucursal: string[];
   totalVentas: number;
   totalCompras: number;
   balance: number;
