b8e88b81f1fa01e2d73e50f8e3fe735ec75d729f
imSoft <weareimsoft@gmail.com>
2023-05-09 23:22:34 -0600

    Se hicieron pruebas con useForm

--- pages/pruebas/index.tsx
+++ pages/pruebas/index.tsx
@@ -1,99 +1,378 @@
-import jsPDF from "jspdf";
-import React from "react";
+import React, { useContext } from "react";
 
-const PruebasPdf = () => {
-  const tiempoActual = Date.now();
-  const hoy = new Date(tiempoActual);
-  const anio = hoy.getFullYear();
-  const mes = hoy.getMonth() + 1;
-  const dia = hoy.getDate();
+import { SidebarLayout } from "../../components/layouts/SidebarLayout";
+import { Resolver, useForm } from "react-hook-form";
+import { ExclamationCircleIcon } from "@heroicons/react/outline";
+import { PersonalesDeMantenimientoContext } from "../../context/gerencia-operativa/personalDeMantenimiento";
+import { useRouter } from "next/router";
 
-  // const hours = hoy.getHours() < 10 ? `0${hoy.getHours()}` : hoy.getHours();
-  // const minutes = hoy.getMinutes() < 10 ? `0${hoy.getMinutes()}` : hoy.getMinutes();
-  // const seconds = hoy.getSeconds() < 10 ? `0${hoy.getSeconds()}` : hoy.getSeconds();
-  // const milliseconds = hoy.getMilliseconds();
-
-  const timeString = hoy.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
-  });
-
-  const lineHeight = 15;
-  const numItems = 1;
-  let ticketHeight = 0;
-
-  if (numItems >= 1) {
-    ticketHeight = numItems * 15 + 155;
-  }
-
-  const doc = new jsPDF({
-    unit: "mm",
-    format: [ticketHeight, 70],
-    putOnlyUsedFonts: true,
-  });
-
-  doc.setLineHeightFactor(1.2);
-  doc.setFontSize(10);
-
-  doc.addImage(
-    "/static/Imagen-ticket.jpg",
-    "JPEG",
-    5,
-    5,
-    60,
-    60
-  );
-
-  // doc.setFont("helvetica", "bold");
-  // doc.text("LA CASA DEL PASTE", 17, 55);
-  // doc.text("TRADACIONAL HIDALGUENSE", 8, 60);
-
-  doc.setFont("helvetica", "normal");
-  doc.text("Av. Conchita #3180", 19, 70);
-  doc.text("Col. Loma Bonita C.P.45086", 12, 75);
-  doc.text("Zapopan,Jal.", 24, 80);
-
-  doc.text("Cel: 33 2967 6329", 20, 90);
-  doc.text("Tel: 33 9688 9088", 20, 95);
-
-  doc.line(3, 100, 67, 100);
-
-  let y = 110;
-
-  for (let i = 1; i <= numItems; i++) {
-    const itemText = `${i} x Frutos rojos con philadelphia`;
-    const itemPrice = "$9,999.99";
-
-    doc.text(itemText, 5, y);
-    doc.text(itemPrice, 48, y + 5);
-
-    y += lineHeight;
-  }
-
-  doc.line(3, y - 2, 67, y - 2);
-
-  doc.setFont("helvetica", "bold");
-  doc.text("TOTAL: ", 5, y + lineHeight - 10);
-  doc.text("$9,999", 54, y + lineHeight - 10);
-
-  doc.line(3, y + lineHeight - 5, 67, y + lineHeight - 5);
-
-  doc.setFont("helvetica", "normal");
-  doc.text("¡Gracias por tu compra!", 16, y + lineHeight * 2 - 10);
-  doc.text("Vuelve pronto", 24, y + lineHeight * 2 - 5);
-
-  doc.text(
-    `${dia < 10 ? "0" + dia : dia}/${
-      mes < 10 ? "0" + mes : mes
-    }/${anio}  ${timeString}`,
-    17,
-    y + lineHeight * 3 - 10
-  );
-
-  doc.save("Ticket de venta.pdf");
-
-  return <div>PruebasPdf</div>;
+type FormData = {
+  nombre: string;
+  oficio: string;
+  direccion: string;
+  telefono: string;
+  sucursalOFranquicia: string;
+  fecha: string;
 };
 
-export default PruebasPdf;
+const resolver: Resolver<FormData> = async (values) => {
+  return {
+    values,
+    errors: !values.nombre
+      ? {
+          nombre: {
+            type: "required",
+            message: "El campo nombre es requerido.",
+          },
+        }
+      : !values.oficio
+      ? {
+          oficio: {
+            type: "required",
+            message: "El campo oficio es requerido.",
+          },
+        }
+      : !values.direccion
+      ? {
+          direccion: {
+            type: "required",
+            message: "El campo dirección es requerido.",
+          },
+        }
+      : !values.telefono
+      ? {
+          telefono: {
+            type: "required",
+            message: "El campo télefono es requerido.",
+          },
+        }
+      : values.sucursalOFranquicia === "Seleccione una opción..."
+      ? {
+          sucursalOFranquicia: {
+            type: "required",
+            message: "El campo sucursal o franquicia es requerido.",
+          },
+        }
+      : !values.fecha
+      ? {
+          fecha: {
+            type: "required",
+            message: "El campo fecha es requerido.",
+          },
+        }
+      : {},
+  };
+};
+
+const pruebas = () => {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver });
+
+  const { agregarNuevoPersonalDeMantenimiento } = useContext(
+    PersonalesDeMantenimientoContext
+  );
+
+  const onSave = ({
+    nombre,
+    oficio,
+    direccion,
+    telefono,
+    sucursalOFranquicia,
+    fecha,
+  }: FormData) => {
+    console.log(
+      nombre,
+      oficio,
+      direccion,
+      telefono,
+      sucursalOFranquicia,
+      fecha
+    );
+
+    agregarNuevoPersonalDeMantenimiento(
+      nombre,
+      oficio,
+      direccion,
+      telefono,
+      true
+    );
+
+    router.push(
+      "/gerencia-operativa/personalMantenimiento/VerPersonalMantenimiento"
+    );
+  };
+
+  return (
+    <SidebarLayout>
+      <form onSubmit={handleSubmit(onSave)}>
+        <div className="shadow sm:rounded-md sm:overflow-hidden">
+          <div className="bg-white py-6 px-4 space-y-6 sm:p-6">
+            <div>
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Pruebas de useForm
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">¡Hola!</p>
+            </div>
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="TxtNombre"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Nombre
+                </label>
+                <div className="relative rounded-md shadow-sm">
+                  <input
+                    type="text"
+                    id="TxtNombre"
+                    autoComplete="off"
+                    className={`${
+                      errors?.nombre
+                        ? "block mt-1 w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+                        : "block mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary-yellow focus:border-primary-yellow sm:text-sm"
+                    }`}
+                    {...register("nombre")}
+                  />
+                  {errors?.nombre && (
+                    <>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                {errors?.nombre && (
+                  <>
+                    <p className="mt-2 text-sm text-red-600" id="email-error">
+                      {errors.nombre.message}
+                    </p>
+                  </>
+                )}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="TxtOficio"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Oficio
+                </label>
+                <div className="relative rounded-md shadow-sm">
+                  <input
+                    type="text"
+                    id="TxtOficio"
+                    autoComplete="off"
+                    className={`${
+                      errors?.oficio
+                        ? "block mt-1 w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+                        : "block mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary-yellow focus:border-primary-yellow sm:text-sm"
+                    }`}
+                    {...register("oficio")}
+                  />
+                  {errors?.oficio && (
+                    <>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                {errors?.oficio && (
+                  <>
+                    <p className="mt-2 text-sm text-red-600" id="email-error">
+                      {errors.oficio.message}
+                    </p>
+                  </>
+                )}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="TxtDireccion"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Dirección
+                </label>
+                <div className="relative rounded-md shadow-sm">
+                  <input
+                    type="text"
+                    id="TxtDireccion"
+                    autoComplete="off"
+                    className={`${
+                      errors?.direccion
+                        ? "block mt-1 w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+                        : "block mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary-yellow focus:border-primary-yellow sm:text-sm"
+                    }`}
+                    {...register("direccion")}
+                  />
+                  {errors?.direccion && (
+                    <>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                {errors?.direccion && (
+                  <>
+                    <p className="mt-2 text-sm text-red-600" id="email-error">
+                      {errors.direccion.message}
+                    </p>
+                  </>
+                )}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="TxtNumeroDeTelefono"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Número de Télefono
+                </label>
+                <div className="relative rounded-md shadow-sm">
+                  <input
+                    type="tel"
+                    id="TxtNumeroDeTelefono"
+                    autoComplete="off"
+                    className={`${
+                      errors?.telefono
+                        ? "block mt-1 w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+                        : "block mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary-yellow focus:border-primary-yellow sm:text-sm"
+                    }`}
+                    {...register("telefono")}
+                  />
+                  {errors?.telefono && (
+                    <>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                {errors?.telefono && (
+                  <>
+                    <p className="mt-2 text-sm text-red-600" id="email-error">
+                      {errors.telefono.message}
+                    </p>
+                  </>
+                )}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="CmbSucursalOFranquicia"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  ¿Sucursal o Franquicia?
+                </label>
+
+                <div className="relative rounded-md shadow-sm">
+                  <select
+                    id="CmbSucursalOFranquicia"
+                    className={`${
+                      errors?.sucursalOFranquicia
+                        ? "block mt-1 w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+                        : "block mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary-yellow focus:border-primary-yellow sm:text-sm"
+                    }`}
+                    defaultValue="Selecciona un producto..."
+                    {...register("sucursalOFranquicia")}
+                  >
+                    <option value={"Seleccione una opción..."} hidden>
+                      Seleccione una opción...
+                    </option>
+                    <option value={"Sucursal"}>Sucursal</option>
+                    <option value={"Franquicia"}>Franquicia</option>
+                  </select>
+
+                  {errors?.sucursalOFranquicia && (
+                    <>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-9">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                {errors?.sucursalOFranquicia && (
+                  <>
+                    <p className="mt-2 text-sm text-red-600" id="email-error">
+                      {errors.sucursalOFranquicia.message}
+                    </p>
+                  </>
+                )}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="TxtFechaDeCompra"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Fecha de Compra
+                </label>
+                <div className="relative rounded-md shadow-sm">
+                  <input
+                    type="date"
+                    id="TxtFechaDeCompra"
+                    className={`${
+                      errors?.fecha
+                        ? "block mt-1 w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+                        : "block mt-1 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary-yellow focus:border-primary-yellow sm:text-sm"
+                    }`}
+                    {...register("fecha")}
+                  />
+                  {errors?.fecha && (
+                    <>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <ExclamationCircleIcon
+                          className="h-5 w-5 text-red-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+                {errors?.fecha && (
+                  <>
+                    <p className="mt-2 text-sm text-red-600" id="email-error">
+                      {errors.fecha.message}
+                    </p>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <button
+              type="submit"
+              className="bg-primary-blue border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-primary-yellow hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-yellow"
+              // onClick={onSave}
+            >
+              Guardar
+            </button>
+          </div>
+        </div>
+      </form>
+    </SidebarLayout>
+  );
+};
+
+export default pruebas;
