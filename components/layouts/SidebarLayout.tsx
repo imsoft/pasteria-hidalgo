f284dbaef83fa421d0a1256e2d7c959ceb270c92
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2023-06-22 10:30:48 -0600

    Se agrego rol de admin

--- components/layouts/SidebarLayout.tsx
+++ components/layouts/SidebarLayout.tsx
@@ -152,7 +152,7 @@ export const SidebarLayout: React.FC<Props> = ({ children }) => {
                       <div
                         key={item.name}
                         className={`${
-                          user?.role === item.role ? "" : "hidden"
+                          user?.role === item.role || 'admin' ? "" : "hidden"
                         }`}
                       >
                         <Link
@@ -208,7 +208,7 @@ export const SidebarLayout: React.FC<Props> = ({ children }) => {
               {navigation.map((item) => (
                 <div
                   key={item.name}
-                  className={`${user?.role === item.role ? "" : "hidden"}`}
+                  className={`${user?.role === item.role || 'admin' ? "" : "hidden"}`}
                 >
                   <Link
                     href={item.href}
