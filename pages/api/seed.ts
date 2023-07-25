3c25a4d5091ea98ee7f6c9b8156b298056359272
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2023-07-25 08:14:37 -0600

    Se borraran registros en DB menos los usuarios

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -39,70 +39,58 @@ export default async function handler(
   await db.connect();
 
   await Candidato.deleteMany(); //Borra todo de la DB
-  await Candidato.insertMany(seedDatabase.initialData.candidatos);
+  // await Candidato.insertMany(seedDatabase.initialData.candidatos);
 
   await PersonalActivo.deleteMany(); //Borra todo de la DB
-  await PersonalActivo.insertMany(seedDatabase.initialData.personalActivo);
+  // await PersonalActivo.insertMany(seedDatabase.initialData.personalActivo);
 
   await CheckInDePersonal.deleteMany(); //Borra todo de la DB
-  await CheckInDePersonal.insertMany(
-    seedDatabase.initialData.checkInDePersonal
-  );
+  // await CheckInDePersonal.insertMany(seedDatabase.initialData.checkInDePersonal);
 
   await AcondicionamientoDeSucursal.deleteMany(); //Borra todo de la DB
-  await AcondicionamientoDeSucursal.insertMany(
-    seedDatabase.initialData.acondicionamientoDeSucursal
-  );
+  // await AcondicionamientoDeSucursal.insertMany(seedDatabase.initialData.acondicionamientoDeSucursal);
 
   await Proveedor.deleteMany(); //Borra todo de la DB
-  await Proveedor.insertMany(seedDatabase.initialData.proveedor);
+  // await Proveedor.insertMany(seedDatabase.initialData.proveedor);
 
   await ReporteDeCompra.deleteMany(); //Borra todo de la DB
-  await ReporteDeCompra.insertMany(seedDatabase.initialData.reporteDeCompra);
+  // await ReporteDeCompra.insertMany(seedDatabase.initialData.reporteDeCompra);
 
   await AsignarPrecio.deleteMany(); //Borra todo de la DB
-  await AsignarPrecio.insertMany(seedDatabase.initialData.asignarPrecio);
+  // await AsignarPrecio.insertMany(seedDatabase.initialData.asignarPrecio);
 
   await ClienteFrecuente.deleteMany(); //Borra todo de la DB
-  await ClienteFrecuente.insertMany(seedDatabase.initialData.clienteFrecuente);
+  // await ClienteFrecuente.insertMany(seedDatabase.initialData.clienteFrecuente);
 
   await ReporteVentasAmbulantesIndividual.deleteMany(); //Borra todo de la DB
-  await ReporteVentasAmbulantesIndividual.insertMany(
-    seedDatabase.initialData.reporteVentasAmbulantesIndividual
-  );
+  // await ReporteVentasAmbulantesIndividual.insertMany(seedDatabase.initialData.reporteVentasAmbulantesIndividual);
 
   await ReporteVentasIndividual.deleteMany(); //Borra todo de la DB
-  await ReporteVentasIndividual.insertMany(
-    seedDatabase.initialData.reporteVentasIndividual
-  );
+  // await ReporteVentasIndividual.insertMany(seedDatabase.initialData.reporteVentasIndividual);
 
   await ApartadoJuridico.deleteMany(); //Borra todo de la DB
-  await ApartadoJuridico.insertMany(seedDatabase.initialData.apartadoJuridico);
+  // await ApartadoJuridico.insertMany(seedDatabase.initialData.apartadoJuridico);
 
   await SucursalYFranquicia.deleteMany(); //Borra todo de la DB
-  await SucursalYFranquicia.insertMany(
-    seedDatabase.initialData.sucursalYFranquicia
-  );
+  // await SucursalYFranquicia.insertMany(seedDatabase.initialData.sucursalYFranquicia);
 
   await PersonalDeMantenimiento.deleteMany(); //Borra todo de la DB
-  await PersonalDeMantenimiento.insertMany(
-    seedDatabase.initialData.personalDeMantenimiento
-  );
+  // await PersonalDeMantenimiento.insertMany(seedDatabase.initialData.personalDeMantenimiento);
 
   await Mantenimiento.deleteMany(); //Borra todo de la DB
-  await Mantenimiento.insertMany(seedDatabase.initialData.mantenimiento);
+  // await Mantenimiento.insertMany(seedDatabase.initialData.mantenimiento);
 
   await ReporteDeSalida.deleteMany(); //Borra todo de la DB
-  await ReporteDeSalida.insertMany(seedDatabase.initialData.reporteDeSalida);
+  // await ReporteDeSalida.insertMany(seedDatabase.initialData.reporteDeSalida);
 
   await MateriaPrima.deleteMany(); //Borra todo de la DB
-  await MateriaPrima.insertMany(seedDatabase.initialData.materiaPrima);
+  // await MateriaPrima.insertMany(seedDatabase.initialData.materiaPrima);
 
   await AsignarComision.deleteMany(); //Borra todo de la DB
-  await AsignarComision.insertMany(seedDatabase.initialData.asignarComision);
+  // await AsignarComision.insertMany(seedDatabase.initialData.asignarComision);
 
   await ManejoDeAlmacen.deleteMany(); //Borra todo de la DB
-  await ManejoDeAlmacen.insertMany(seedDatabase.initialData.manejoDeAlmacen);
+  // await ManejoDeAlmacen.insertMany(seedDatabase.initialData.manejoDeAlmacen);
 
   await Usuario.deleteMany(); //Borra todo de la DB
   await Usuario.insertMany(seedDatabase.initialData.usuario);
